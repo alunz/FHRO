b9d9271a17c573dd2cd7751c99ea258e522f9d59
Sebastian Springer <sebastian.springer@mayflower.de>
2014-05-06 08:18:13 +0200

    fixed the backend

--- addressBook/Gruntfile.js
+++ addressBook/Gruntfile.js
@@ -1,3 +1,46 @@
-/**
- * Created by sspringer on 15/04/14.
- */
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        copy: {
+            bower: {
+                files: [
+                    {
+                        expand: true,
+                        src: 'bower_components/backbone/backbone.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/underscore/underscore.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/jquery/dist/jquery.min.js',
+                        dest: 'public/js/lib',
+                        flatten: true,
+                        rename: function(dest, src) {
+                            return dest + '/' + src.replace('.min', '');
+                        }
+                    }
+                ]
+            }
+        },
+
+        clean: {
+            bower: ['bower_components']
+        },
+
+        shell: {
+            bower: {
+                command: 'bower install'
+            }
+        }
+
+
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('bower', ['shell:bower', 'copy:bower', 'clean:bower']);
+}
--- addressBook/index.js
+++ addressBook/index.js
@@ -1,3 +1,20 @@
-/**
- * Created by sspringer on 31/03/14.
- */
+var express = require('express');
+
+var routes = require('./routes.js');
+
+var app = express();
+
+app.use(express.bodyParser());
+
+routes(app);
+
+app.use('/', express.static(__dirname + '/public/html'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/style', express.static(__dirname + '/public/style'));
+
+app.listen(8080);
+
+// curl -X GET http://localhost:8080/address
+// curl -X POST -d "gender=male&firstname=Klaus&lastname=Schmidt&street=Hauptstraße 1&postcode=67890&place=München" http://localhost:8080/address
+// curl -X PUT -d "gender=female&firstname=Lisa&lastname=Müller&street=Dorfstraße 2&postcode=24680&place=Berlin" http://localhost:8080/address/id/2
+// curl -X DELETE http://localhost:8080/address/id/2
