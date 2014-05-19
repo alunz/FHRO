800f6c6df90186377ec297bac37f7f7c2e8d1b81
Sebastian Springer <sebastian.springer@mayflower.de>
2014-05-19 22:10:04 +0200

    Angular Advanced

--- addressBook/Gruntfile.js
+++ addressBook/Gruntfile.js
@@ -9,7 +9,38 @@ module.exports = function(grunt) {
                         src: 'bower_components/angular/angular.js',
                         dest: 'public/js/lib',
                         flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/angular-translate/angular-translate.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/angular-ui-router/release/angular-ui-router.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/domReady/domReady.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/requirejs/require.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/text/text.js',
+                        dest: 'public/js/lib',
+                        flatten: true
+                    },{
+                        expand: true,
+                        src: 'bower_components/angular-ui-bootstrap/src/modal/modal.js',
+                        dest: 'public/js/lib',
+                        flatten: true
                     }
+
                 ]
             }
         },
