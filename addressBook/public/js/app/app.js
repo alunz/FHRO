0d48f3335d032b3c52ebd86ef41cc425773cd0e9
Sebastian Springer <sebastian.springer@mayflower.de>
2014-05-20 11:25:06 +0200

    Routing

--- addressBook/public/js/app/app.js
+++ addressBook/public/js/app/app.js
@@ -1,5 +1,7 @@
-define(['angular', 'ngTrans', 'ngRouter', 'modal', 'translations/de', 'translations/en'], function(angular, ngTrans, ngRouter, modal, de, en) {
-    var addressBook = angular.module('addressBook', ['pascalprecht.translate']);
+define(
+    ['angular', 'ngTrans', 'ngRouter', 'modal', 'translations/de', 'translations/en', 'text!../../list.html', 'text!../../form.html'],
+    function(angular, ngTrans, ngRouter, modal, de, en, tplList, tplForm) {
+    var addressBook = angular.module('addressBook', ['pascalprecht.translate', 'ui.router']);
 
     addressBook.config(['$translateProvider', function ($translateProvider) {
         $translateProvider.translations('de', de);
@@ -7,14 +9,44 @@ define(['angular', 'ngTrans', 'ngRouter', 'modal', 'translations/de', 'translati
         $translateProvider.preferredLanguage('en');
     }]);
 
-    addressBook.controller('addressBookCtrl', ['$scope', '$http', '$translate', function($scope, $http, $translate) {
+    addressBook.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/list');
+        $stateProvider.state('list', {
+            url: '/list',
+            template: tplList,
+            controller: 'listCtrl'
+        });
 
+        $stateProvider.state('delete', {
+            url: '/delete/:id',
+            controller: 'deleteCtrl'
+        });
+
+        $stateProvider.state('edit', {
+            url: '/edit/:id',
+            template: tplForm,
+            controller: 'formCtrl'
+        });
+    }]);
+
+    addressBook.controller('translateCtrl', ['$scope', '$translate', function($scope, $translate) {
         $scope.translate = function(lang) {
             $translate.use(lang);
         };
+    }]);
 
-        $scope.displayForm = false;
+    addressBook.controller('listCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.addresses = [];
+        $http.get('/address').success(function (data) {
+            $scope.addresses = data;
+        });
+    }]);
 
+    addressBook.controller('deleteCtrl', ['$scope', '$http', '$stateParams', '$state', function($scope, $http, $stateParams, $state) {
+        $http.delete('/address/' + $stateParams.id).success($state.go.bind($state, 'list'));
+    }]);
+
+    addressBook.controller('formCtrl', ['$scope', '$http', '$stateParams', '$state', function($scope, $http, $stateParams, $state) {
         $scope.address = {
             id: '',
             gender: '',
@@ -25,53 +57,19 @@ define(['angular', 'ngTrans', 'ngRouter', 'modal', 'translations/de', 'translati
             place: ''
         };
 
-        $scope.addresses = [];
-
-        $scope.edit = function(id) {
-            $http.get('/address/' + id).success(function(data) {
+        if($stateParams.id) {
+            $http.get('/address/' + $stateParams.id).success(function(data) {
                 $scope.address = data;
             });
-            $scope.showForm();
-        };
-
-        $scope.new = function() {
-            for (var i in $scope.address) {
-                if ($scope.address.hasOwnProperty(i)) {
-                    $scope.address[i] = '';
-                }
-            }
-            $scope.showForm();
-        };
-
-        $scope.showForm = function () {
-            $scope.displayForm = true;
-        };
-
-        $scope.hideForm = function () {
-            $scope.displayForm = false;
-        };
-
-        // persistance
-        $scope.fetch = function () {
-            $http.get('/address').success(function (data) {
-                $scope.addresses = data;
-                $scope.hideForm();
-            });
-        };
-
-        $scope.delete = function (id) {
-            $http.delete('/address/' + id).success($scope.fetch);
-        };
+        }
 
         $scope.save = function () {
             if ($scope.address.id) {
-                $http.put('/address/' + $scope.address.id, $scope.address).success($scope.fetch);
+                $http.put('/address/' + $scope.address.id, $scope.address).success($state.go.bind($state, 'list'));
             } else {
-                $http.post('/address', $scope.address).success($scope.fetch);
+                $http.post('/address', $scope.address).success($state.go.bind($state, 'list'));
             }
         };
 
-        // initialize
-        $scope.fetch();
     }]);
 });
